fc4ae2cd8c70d3fd23bc1d796330335b13863195
SChoudhary <schoudhary@esolutionsfirst.com>
2022-09-03 04:34:27 -0400

    get url from env instead of cloudformation

--- 02postman/update-postman-env-file.sh
+++ 02postman/update-postman-env-file.sh
@@ -5,9 +5,7 @@
 
 echo "Running update-postman-env-file.sh"
 
-api_gateway_url=`aws cloudformation describe-stacks \
-  --stack-name petstore-api-stack \
-  --query "Stacks[0].Outputs[*].{OutputValueValue:OutputValue}" --output text`
+api_gateway_url=${APP_URL}
 
 echo "API Gateway URL:" ${api_gateway_url}
 
